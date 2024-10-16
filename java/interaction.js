ad73eca9cc4db42c8f6b93f9dcb5e45bb3b9899c
suctuk <seuc9452@colorado.edu>
2024-10-15 19:22:51 -0600

    Added cursor animations. And other stuff. Love yall!

---
+++ java/interaction.js
@@ -0,0 +1,9 @@
+document.addEventListener('mousemove', (e) => {
+  let cursor = document.createElement('div');
+  cursor.classList.add('cursor');
+  cursor.style.left = `${e.pageX}px`;
+  cursor.style.top = `${e.pageY}px`;
+  document.body.appendChild(cursor);
+
+  setTimeout(() => cursor.remove(), 1000);
+});
